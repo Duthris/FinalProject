236882bd9568203bde1635b47332b087e7b04a12
ADEM ANIL YALIMDEMİR <adem.yalimdemir@std.antalya.edu.tr>
2021-05-20 16:01:55 +0300

    Add project files.

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+using Core.Entities.Concrete;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün Eklendi.";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz!";
+        public static string MaintenanceTime = "Bakım zamanı!";
+        public static string ProductsListed = "Ürünler Listelendi.";
+        public static string ProductCountError = "Kategoride bulunan ürün sayısı limiti aşıyor!";
+        public static string ProductUpdated = "Ürün güncellendi.";
+        public static string ProductNameAlreadyExists = "Ürün adı zaten mevcut!";
+        public static string CategoryLimitExceded = "Kategori limiti aşıldı.";
+        public static string AuthorizationDenied = "Doğrulama Reddedildi. YETKİNİZ YOK!";
+        public static string UserRegistered = "Başarıyla kayıt olundu!";
+        public static string UserNotFound = "Kullanıcı bulunamadı!";
+        public static string PasswordError = "Geçersiz şifre!";
+        public static string SuccessfulLogin = "Giriş başarılı!";
+        public static string UserAlreadyExists = "Kullanıcı zaten mevcut!";
+        public static string AccessTokenCreated = "Access Token Oluşturuldu.";
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,121 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            ProductTest();
+            CategoryTest();
+            ProductUnitPriceTest();
+            ProductCategoryIdTest();
+            ProductCategoryNameTest();
+
+            Console.ReadLine();
+        }
+
+        private static void ProductCategoryNameTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal(), new CategoryManager(new EfCategoryDal()));
+
+            var result = productManager.GetProductDetails();
+
+            if (result.Success == true)
+            {
+                Console.WriteLine("\n############ SİSTEMDE KAYITLI ÜRÜN / KATEGORİ LİSTESİ ############\n");
+
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine("{0}: {1} / {2}\n", product.ProductId, product.ProductName, product.CategoryName);
+                }
+            }
+
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+        }
+
+        private static void ProductCategoryIdTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal(), new CategoryManager(new EfCategoryDal()));
+
+            var result = productManager.GetAllByCategoryId(2);
+
+            if (result.Success == true)
+            {
+                Console.WriteLine("\n############ BELİRTİLEN KATEGORİ NUMARASINA AİT ÜRÜNLER ############\n");
+
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine("{0}: {1}", product.ProductId, product.ProductName);
+                }
+            }
+
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+        }
+
+        private static void ProductUnitPriceTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal(), new CategoryManager(new EfCategoryDal()));
+
+            var result = productManager.GetByUnitPrice(40, 100);
+
+            if (result.Success == true)
+            {
+                Console.WriteLine("\n############ BELİRTİLLEN FİYAT ARALIĞINDAKİ ÜRÜNLER ############\n");
+
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine("{0}: {1}", product.ProductId, product.ProductName);
+                }
+            }
+
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+
+            Console.WriteLine("\n############ SİSTEMDE KAYITLI ÜRÜN KATEGORİLERİ ############\n");
+
+            foreach (var category in categoryManager.GetAll().Data)
+            {
+                Console.WriteLine("{0}: {1}", category.CategoryId, category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal(), new CategoryManager(new EfCategoryDal()));
+
+            var result = productManager.GetAll();
+
+            if (result.Success == true)
+            {
+                Console.WriteLine("############ SİSTEMDE KAYITLI ÜRÜNLER ############\n");
+
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine("{0}: {1}", product.ProductId, product.ProductName);
+                }
+            }
+
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+        }
+    }
+}
